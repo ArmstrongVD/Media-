ceb93fc64e343f3017b7ccfa4dceb8a88bf27743
ArmstrongVD <Dimitri.Armstrong@va.gov>
2024-09-11 08:48:37 -0400

    finished the users list and items

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button.js";
 import useThunk from "../hooks/use-thunk.js";
+import UsersListItem from "./UsersListItem.js";
 
 function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -23,36 +24,30 @@ function UsersList() {
     doCreateUser();
   };
 
+  let content;
+
   if (isLoadingUsers) {
-    return <Skeleton times={6} className="h-10 w-full" />;
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data ...</div>;
+  } else {
+    content = data.map((user) => {
+      return <UsersListItem key={user.id} user={user} />;
+      
+    });
   }
-
-  if (loadingUsersError) {
-    return <div>Error fetching data ...</div>;
-  }
-
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
-        </div>
-      </div>
-    );
-  });
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between item-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? (
-          "Creating User ..."
-        ) : (
-          <Button onClick={handleUserAdd}>+ Add User</Button>
-        )}
+
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
         {creatingUserError && "Error creating User ...."}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
