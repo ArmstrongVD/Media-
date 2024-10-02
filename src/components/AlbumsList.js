6ecf1aac9924fd9dedf919300dd392437674ed03
ArmstrongVD <Dimitri.Armstrong@va.gov>
2024-10-02 08:12:08 -0400

    Added the data fetching query for photos

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { useFetchAlbumsQuery,useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
-import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
+import AlbumListItem from "./AlbumListItem";
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
@@ -13,18 +13,13 @@ function AlbumsList({ user }) {
   };
 
   let content;
-  if (isLoading) {
-    content = <Skeleton times={3} />;
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
     content = data.map((album) => {
-      const header = <div>{album.title}</div>;
-      return (
-        <ExpandablePanel key={album.id} header={header}>
-          List of Photos in the Album
-        </ExpandablePanel>
-      );
+     return <AlbumListItem key={album.id} album={album} />
     });
   }
 
@@ -32,7 +27,7 @@ function AlbumsList({ user }) {
     <div>
       <div className="m-2 flex flex-row items-center justify-between ">
        <h3 className="text-lg font-bold">Albums for {user.name} </h3>
-        <Button onClick={handleAddAlbum}>
+        <Button onClick={handleAddAlbum} loading={results.isLoading}>
           + Add Album
         </Button>
         </div>
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,31 +1,58 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { faker } from "@faker-js/faker";
 
+// Dev Only !!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
 const albumsApi = createApi({
   reducerPath: "albums",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3005",
+    fetchFn: async (...args) => {
+      // remove for prod
+      await pause(1000);
+      return fetch(...args);
+    },
   }),
   endpoints(builder) {
     return {
-      addAlbum: builder.mutation({
-        invalidatesTags:(result, error, user) => {
-          return [{type: 'Album', id: user.id}]
+      removeAlbum: builder.mutation({
+        invalidatesTags: ( result, error, album) => {
+          return [{ type: 'Album', id:album.id }]
         },
-        query:(user) => {
+        query: (album) => {
           return {
-            url: '/albums',
-            method: 'POST',
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
+      addAlbum: builder.mutation({
+        invalidatesTags: (result, error, user) => {
+          return [{ type: "UsersAlbums", id: user.id }];
+        },
+        query: (user) => {
+          return {
+            url: "/albums",
+            method: "POST",
             body: {
-              userId : user.id,
-              title: faker.commerce.productName()
-            }
-          }
-        }
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
       }),
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{ type: 'Album', id: user.id}]
+          const tags = result.map(album => {
+            return { type: 'Album', id: album.id }
+          });
+          tags.push({ type: 'UsersAlbums', id: user.id});
+          return tags;
         },
         query: (user) => {
           return {
@@ -41,5 +68,9 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
